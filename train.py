fe3eda3137bf0bf8a94253ddedf348c04e5b7cef
Seulgi Kim <40836553+skvictoria@users.noreply.github.com>
2020-07-29 17:59:37 +0900

    Update train.py

--- train.py
+++ train.py
@@ -8,48 +8,45 @@ from tensorflow.keras.models import load_model
 
 # first read every single csv files and make one csv file for training only - make function!!!
 
-# 1.csv to (1_normal.csv and 1_lanechng.csv 1_left.csv)
-def seperate_data(tracknum):
+def seperate_data(tracknum, cnt, span):
     rawdf = pd.read_csv('{}.csv'.format(tracknum), index_col=0)
     ansdf = pd.read_csv('{}_ans.csv'.format(tracknum), index_col = 0)
     temp = pd.merge(rawdf, ansdf, left_on="0", left_index = True,right_index = True, how='left').dropna(axis=0)
-    normal = temp[temp['ans']==0]
-    lanechng = temp[temp['ans']==1]
-    left = temp[temp['ans']==2]
-    pd.DataFrame(normal).to_csv('{}_normal.csv'.format(tracknum))
-    pd.DataFrame(lanechng).to_csv('{}_lanechng.csv'.format(tracknum))
-    pd.DataFrame(left).to_csv('{}_left.csv'.format(tracknum))
-
-# in one data folder
-def load_data(cnt, span, group):
-  
-''' 
-def sequential_window_dataset(series, window_size):
-    series = tf.expand_dims(series, axis=-1)
-    ds = tf.data.Dataset.from_tensor_slices(series)
-    ds = ds.window(window_size + 1, shift=window_size, drop_remainder=True)
-    ds = ds.flat_map(lambda window: window.batch(window_size + 1))
-    ds = ds.map(lambda window: (window[:-1], window[1:]))
-    return ds.batch(1).prefetch(1)
-'''
+    data = []
+    y_data = []
+    for i in range(0, len(temp)-cnt):
+        tmplist = []
+        for j in range(i, cnt):
+            tmplist.append(j)
+        if(temp.iloc[i+cnt+span,5]==0):
+            data.append(temp.iloc[tmplist,[0,1,3]].to_numpy())
+            y_data.append(0)
+        elif(temp.iloc[i+cnt+span,5]==1):
+            data.append(temp.iloc[tmplist,[0,1,3]].to_numpy())
+            y_data.append(1)
+        elif(temp.iloc[i+cnt+span,5]==2):
+            data.append(temp.iloc[tmplist,[0,1,3]].to_numpy())
+            y_data.append(2)
+        else:
+            continue
+    n_train = int(len(data) * 0.8)
+    n_test = int(len(data) - n_train)
+    
+    X_test = data[n_train:]
+    y_test = np.array(y_data[n_train:])
+    X_train = data[:n_train]
+    y_train = np.array(y_data[:n_train])
+    
+    return X_train, y_train, X_test, y_test
 
 # window_size = #
-
+'''
 dataset = pd.read_csv('training.csv', index_col=0)
 
 # Extract out states and labels
 states = dataset['vecs'].tolist()
 labels = dataset['state'].tolist()
-
-n_train = int(len(states) * 0.8)
-n_test = int(len(states) - n_train)
-print('num of train datasets :',n_train)
-print('num of test datasets:',n_test)
-
-X_test = data[n_train:]
-y_test = np.array(y_data[n_train:])
-X_train = data[:n_train]
-y_train = np.array(y_data[:n_train])
+'''
 
 model = Sequential()
 # model.add(Embedding(3, 32)) # embedding vector 32 levels
