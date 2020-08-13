c8cb3a3884f7d8ff4e0dfdddbfd063633f0fdfbc
yimju <67643341+yimju@users.noreply.github.com>
2020-08-13 12:50:09 +0900

    Create kitti_fusion.py

---
+++ kitti_fusion.py
@@ -0,0 +1,74 @@
+import numpy as np
+import open3d as o3d
+import time
+import copy
+import loadData
+import cv2
+
+
+RT = np.array([[7.533745e-03, -9.999714e-01, -6.166020e-04, -4.069766e-03],
+               [1.480249e-02, 7.280733e-04, -9.998902e-01, -7.631618e-02],
+               [9.998621e-01, 7.523790e-03, 1.480755e-02, -2.717806e-01],
+               [0.0, 0.0, 0.0, 1.0]])
+
+R_rect_00 = np.array([[9.999239e-01, 9.837760e-03, -7.445048e-03, 0.0],
+                      [-9.869795e-03, 9.999421e-01, -4.278459e-03, 0.0],
+                      [7.402527e-03, 4.351614e-03, 9.999631e-01, 0.0],
+                      [0.0, 0.0, 0.0, 1]])
+
+P_rect_00 = np.array([[7.215377e+02, 0.000000e+00, 6.095593e+02, 0.000000e+00],
+                      [0.000000e+00, 7.215377e+02, 1.728540e+02, 0.000000e+00],
+                      [0.000000e+00, 0.000000e+00, 1.000000e+00, 0.000000e+00]])
+
+Calibration_matrix = P_rect_00 @ R_rect_00 @ RT
+
+
+# path = "./2011_09_26/2011_09_26_drive_0005_sync/"
+path = "/media/yimju/7B22CE2F737C2A58/Lyft_train/26/"
+# path_lidar = path + "velodyne_points/data/"
+path_lidar = path + 'lidar/'
+#path_csv = path + "csvdata/"
+# path_image = path + "image_00/data/"
+path_image = path + "images/"
+
+file_list = loadData.load_data(path_lidar)
+image_list = loadData.load_data(path_image)
+
+cv2.namedWindow('Show Image')
+
+img = cv2.imread(path_image + image_list[0], cv2.IMREAD_COLOR)
+files = file_list[0]
+
+pointcloud = np.fromfile(path_lidar+files,dtype = np.float32)
+pointcloud = pointcloud.reshape(-1,5)
+
+# Cropping
+pointcloud = pointcloud[(pointcloud[:,2] >= -1.40)]
+pointcloud = pointcloud[(pointcloud[:,0] >= 0)]
+pointcloud = pointcloud[(pointcloud[:,0] <= 30)]
+pointcloud = pointcloud[(pointcloud[:,1] >= -10)]
+pointcloud = pointcloud[(pointcloud[:,1] <= 10)]
+
+pointcloud[:,3] = 1.0
+
+
+Y = (Calibration_matrix @ pointcloud.T).T
+
+
+Point_x = Y[:,0] / Y[:,2]
+Point_y = Y[:,1] / Y[:,2]
+
+Point_x = Point_x.astype('int32')
+Point_y = Point_y.astype('int32')
+
+print(Point_x)
+print(Point_y)
+
+for i in range(0, len(Point_x)):
+    cv2.circle(img, (Point_x[i], Point_y[i]), 3, (0, 0, 1), -1)
+
+cv2.imshow("Show Image", img)
+cv2.waitKey(0)
+
+#for files in file_list:
+#    dt = 0.2
