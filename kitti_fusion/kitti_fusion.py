a1080ee64cf0b21ca40a28861cb17222c6c4ef09
yimju <67643341+yimju@users.noreply.github.com>
2020-08-17 01:48:23 +0900

    Update kitti_fusion.py

--- kitti_fusion/kitti_fusion.py
+++ kitti_fusion/kitti_fusion.py
@@ -4,7 +4,7 @@ import time
 import copy
 import loadData
 import cv2
-from checkCoin import checkCoin
+from getIntersecion import GetIntersection
 
 ######################## Initialization Various ########################
 
@@ -28,16 +28,17 @@ Calibration_matrix = P_rect_00 @ R_rect_00 @ RT
 path = "./"
 path_lidar = path + "velodyne_points/data/"
 path_image = path + "image_02/data/"
-
+path_segmap = path + "segmenticmap/"
 file_list = loadData.load_data(path_lidar)
 image_list = loadData.load_data(path_image)
+segmap_list = loadData.load_data(path_segmap)
 
-filename = 'save.bin'
-f = open(filename,'rb')
+# filename = 'save.bin'
+f = open(path_segmap+segmap_list[0],'rb')
 data = f.read()
-semanticmap = np.frombuffer(data, dtype=np.int64)
-semanticmap = semanticmap.reshape(620,2048)
-print(semanticmap)
+semanticMap = np.frombuffer(data, dtype=np.int64)
+# semanticMap = semanticMap.reshape(620,2048)
+semanticMap = semanticMap.reshape(376,1241)
 f.close()
 
 cv2.namedWindow('Show Image')
@@ -49,7 +50,7 @@ cv2.namedWindow('Show Image')
 # files = file_list[0]
 # for i in range(len(file_list)):
 
-i=1
+i=0
 img = cv2.imread(path_image + image_list[i], cv2.IMREAD_COLOR)
 pointcloud = np.fromfile(path_lidar+file_list[i], dtype = np.float32)
 pointcloud = pointcloud.reshape(-1,4)
@@ -62,7 +63,6 @@ pointcloud = pointcloud[(pointcloud[:,1] >= -10)]
 pointcloud = pointcloud[(pointcloud[:,1] <= 10)]
 
 pointcloud[:,3] = 1.0
-
 start = time.time()
 Y = (Calibration_matrix @ pointcloud.T).T
 print(time.time() - start)
@@ -70,34 +70,23 @@ print(time.time() - start)
 Point_x = Y[:,0] / Y[:,2]
 Point_y = Y[:,1] / Y[:,2]
 
-Point_x = Point_x.astype('int32')
-Point_y = Point_y.astype('int32')
+Point_x = Point_x.astype('int64')
+Point_y = Point_y.astype('int64')
 
 Point_x = Point_x.reshape(-1,1)
 Point_y = Point_y.reshape(-1,1)
 lidar2pixel = np.append(Point_x,Point_y, axis =1)
-# print(len(lidar2pixel))
-# print(Point_y)
 
 # start = time.time()
 for i in range(0, len(Point_x)):
     cv2.circle(img, (Point_x[i], Point_y[i]), 1, (0, 0, 255), -1)
 # print(time.time() - start)
 
-# print(semanticmap.shape)
-# print(lidar2pixel.shape)
-fusion_index = checkCoin(lidar2pixel,semanticmap,24)
-
-pointcloud_3d = pointcloud[:,0:2]
-roadpoint = pointcloud_3d[fusion_index,:]
-roadpoint2pixel = lidar2pixel[fusion_index,:]
-
-# for i in range(0, len(semanticmap)):
-#     print(semanticmap[i][:])
+road_pixel = GetIntersection(lidar2pixel,semanticMap,24)
 
 
-for i in range(0, len(roadpoint2pixel)):
-    cv2.circle(img, (roadpoint2pixel[i][0], roadpoint2pixel[i][1]), 1, (0, 255, 0), -1)
+for i in range(0, len(road_pixel)):
+    cv2.circle(img, (road_pixel[i][0], road_pixel[i][1]), 1, (0, 255, 0), -1)
 
 # cv2.circle(img,(1,1), 1, (255, 255, 0), -1)
 cv2.imshow("Show Image", img)
